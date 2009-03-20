4f76e874a99f7afc209b7e4328d96a449814766a
Boba Fat <ivan@workisfun.ru>
2009-03-20 21:52:49 +0300

    added all clear actions

--- rdoc_all.rb
+++ rdoc_all.rb
@@ -95,18 +95,24 @@ end
 
 class RdocAll::Ruby < RdocAll::Base
   class << self
-    def each(&block)
-      Dir['ruby-*'].select{ |path| File.directory?(path) }.each(&block)
+    def tars
+      Dir['ruby-*.tar.bz2']
+    end
+
+    def rubys
+      Dir['ruby-*'].select{ |path| File.directory?(path) }
     end
 
     def update_sources(options = {})
+      to_clear = tars
       Net::FTP.open('ftp.ruby-lang.org') do |ftp|
         ftp.debug_mode = true
         ftp.passive = true
         ftp.login
         ftp.chdir('/pub/ruby')
-        ftp.list('ruby*.tar.bz2').each do |line|
+        ftp.list('ruby-*.tar.bz2').each do |line|
           tar_path, tar = File.split(line.split.last)
+          to_clear.delete(tar)
           remove_if_present(tar) if options[:force]
           unless File.exist?(tar)
             ftp.chdir('/pub/ruby' / tar_path)
@@ -114,18 +120,26 @@ class RdocAll::Ruby < RdocAll::Base
           end
         end
       end
+      to_clear.each do |tar|
+        remove_if_present(tar)
+      end
 
-      Dir['ruby-*.tar.bz2'].each do |tar|
+      to_clear = rubys
+      tars.each do |tar|
         ruby = File.basename(tar, '.tar.bz2')
+        to_clear.delete(ruby)
         remove_if_present(ruby) if options[:force]
         unless File.directory?(ruby)
           system('tar', '-xjf', tar)
         end
       end
+      to_clear.each do |ruby|
+        remove_if_present(ruby)
+      end
     end
 
     def add_rdoc_tasks
-      each do |ruby|
+      rubys.each do |ruby|
         add_rdoc_task(ruby)
       end
     end
@@ -158,13 +172,18 @@ class RdocAll::Rails < RdocAll::Base
     end
 
     def update_sources(options = {})
+      to_clear = Dir['rails-*']
       each do |rails, version|
+        to_clear.delete(rails)
         remove_if_present(rails) if options[:force]
         unless File.directory?(rails)
           with_env 'VERSION', version do
             system('rails', rails, '--freeze')
           end
         end
+      end
+      to_clear.each do |rails|
+        remove_if_present(rails)
       end
     end
 
@@ -217,5 +236,5 @@ class RdocAll::Plugins < RdocAll::Base
   end
 end
 
-# RdocAll.update_sources
-RdocAll.build_documentation
+RdocAll.update_sources
+# RdocAll.build_documentation
