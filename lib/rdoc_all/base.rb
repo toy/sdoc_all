a14138cc066d747b05d21ab34a73c3d0315ed90c
Boba Fat <ivan@workisfun.ru>
2009-03-21 13:25:50 +0300

    extracted utility methods from base

--- lib/rdoc_all/base.rb
+++ lib/rdoc_all/base.rb
@@ -1,10 +1,10 @@
-class RdocAll::Base
-  class << self
-    def inherited(subclass)
+class RdocAll
+  class Base
+    def self.inherited(subclass)
       (@subclasses ||= []) << subclass
     end
 
-    def update_all_sources(options = {})
+    def self.update_all_sources(options = {})
       Dir.chdir(SOURSES_PATH) do
         @subclasses.each do |subclass|
           subclass.update_sources(options)
@@ -12,47 +12,36 @@ class RdocAll::Base
       end
     end
 
-    def rdoc_tasks(options = {})
+    def self.rdoc_tasks(options = {})
       Dir.chdir(SOURSES_PATH) do
-        @@rdoc_tasks = []
+        @@tasks = RdocTasks.new
 
         @subclasses.each do |subclass|
           subclass.add_rdoc_tasks
         end
 
         to_clear = Dir.glob(DOCS_PATH / '*' / '*')
-        @@rdoc_tasks.each do |rdoc_task|
-          doc_path = DOCS_PATH / rdoc_task[:base_path]
-          to_clear.delete_if{ |path| path[0, doc_path.length] == doc_path }
+        @@tasks.each do |task|
+          doc_path = DOCS_PATH / task.base_path
+          to_clear.delete_if{ |path| path.starts_with?(doc_path) }
         end
         to_clear.each do |path|
           remove_if_present(path)
         end
 
-        @@rdoc_tasks.each do |rdoc_task|
-          doc_path = DOCS_PATH / rdoc_task[:base_path]
+        @@tasks.each do |task|
+          doc_path = DOCS_PATH / task.base_path
           remove_if_present(doc_path) if Dir[doc_path / '*'].empty? || options[:force]
         end
 
-        @@rdoc_tasks
+        @@tasks
       end
     end
 
   protected
 
-    def add_rdoc_task(base_path, source_pathes = [])
-      @@rdoc_tasks << {:base_path => base_path, :source_pathes => source_pathes, :title => base_path.sub('s/', ' — ')}
-    end
-
-    def with_env(key, value)
-      old_value, ENV[key] = ENV[key], value
-      yield
-    ensure
-      ENV[key] = old_value
-    end
-
-    def remove_if_present(path)
-      FileUtils.remove_entry(path) if File.exist?(path)
+    def self.add_rdoc_task(base_path, pathes = [])
+      @@tasks.add(self, base_path, pathes)
     end
   end
 end
